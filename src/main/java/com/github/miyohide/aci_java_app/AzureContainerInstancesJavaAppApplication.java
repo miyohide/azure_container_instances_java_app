743088862b700b1c14834e48de4d1c51779a15e7
Hidenori Miyoshi <miyohide@gmail.com>
2021-12-11 17:16:20 +0900

    initial commit

---
+++ src/main/java/com/github/miyohide/aci_java_app/AzureContainerInstancesJavaAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.aci_java_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureContainerInstancesJavaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureContainerInstancesJavaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/miyohide/aci_java_app/AzureContainerInstancesJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.aci_java_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureContainerInstancesJavaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
