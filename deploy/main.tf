3d455545aa5fc424787c80209650e40da549ada2
Hidenori Miyoshi <miyohide@gmail.com>
2021-12-14 21:36:09 +0900

    update terraform script that create vnet and subnet.

--- deploy/main.tf
+++ deploy/main.tf
@@ -7,3 +7,18 @@ resource "azurerm_resource_group" "rg" {
   location = var.rg_location
   name     = var.rg_name
 }
+
+# VNETの作成
+resource "azurerm_virtual_network" "vnet" {
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  name                = "aciJavaAppVnet"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnetの作成
+resource "azurerm_subnet" "subnet1" {
+  name                 = "aciJavaAppSubnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+}
