ae7b72ea0beec37c029513fbbc44eb0a66c08d4a
Hidenori Miyoshi <miyohide@gmail.com>
2021-12-17 21:26:12 +0900

    add subnet config

--- deploy/01_prepare/main.tf
+++ deploy/01_prepare/main.tf
@@ -21,6 +21,15 @@ resource "azurerm_subnet" "subnet1" {
   name                 = "aciJavaAppSubnet1"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = ["10.0.0.0/24"]
+
+  delegation {
+    name = "delegation"
+    service_delegation {
+      name = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
 }
 
 # Azure Container Registryの作成
