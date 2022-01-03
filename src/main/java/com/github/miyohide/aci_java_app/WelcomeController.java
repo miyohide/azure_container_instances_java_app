be18f9fbee46cc681942d25a0a267c21f414e249
Hidenori Miyoshi <miyohide@gmail.com>
2022-01-03 20:57:39 +0900

    replacce getHostAddress

--- src/main/java/com/github/miyohide/aci_java_app/WelcomeController.java
+++ src/main/java/com/github/miyohide/aci_java_app/WelcomeController.java
@@ -4,14 +4,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.net.Inet4Address;
-import java.net.UnknownHostException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 @Controller
 public class WelcomeController {
     @GetMapping("/")
-    public String index(Model model) throws UnknownHostException {
-        model.addAttribute("server_ip_address", Inet4Address.getLocalHost().getHostAddress());
+    public String index(Model model) throws SocketException {
+        List<String> addresses = new ArrayList<>();
+        /* IPアドレスの取得処理。 InetAddress.getLocalHost().getHostAddress()
+        * を実行した場合、127.0.0.1だけが返ってくることがあるので、それを防ぐために
+        * ネットワークインターフェイスを取得し、それぞれにおいてIPアドレスを取得する
+        * 実装としている。
+        */
+        Enumeration<NetworkInterface> n = NetworkInterface.getNetworkInterfaces();
+        while (n.hasMoreElements()) {
+            NetworkInterface e = n.nextElement();
+            Enumeration<InetAddress> a = e.getInetAddresses();
+            while (a.hasMoreElements()) {
+                InetAddress addr = a.nextElement();
+                addresses.add("Display Name = [" + e.getDisplayName() + "], Host Address = [" + addr.getHostAddress() + "]");
+            }
+        }
+        model.addAttribute("addresses", addresses);
         return "index";
     }
 }
