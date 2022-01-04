24497f9dafc5d6a2c7f0274e71c3c1cd8083aa2e
Hidenori Miyoshi <miyohide@gmail.com>
2022-01-04 20:43:56 +0900

    delete command that create container

--- deploy/create_storage.sh
+++ deploy/create_storage.sh
@@ -2,8 +2,7 @@
 
 RESOURCE_GROUP_NAME=tfstate
 STORAGE_ACCOUNT_NAME=tfstate$RANDOM
-CONTAINER_NAME_01_PREPARE=tfstate01prepare
-CONTAINER_NAME_02_EXEC=tfstate02exec
+CONTAINER_NAME=tfstate
 
 # Create resource group
 az group create --name $RESOURCE_GROUP_NAME --location eastus
@@ -12,5 +11,4 @@ az group create --name $RESOURCE_GROUP_NAME --location eastus
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
 
 # Create blob container
-az storage container create --name $CONTAINER_NAME_01_PREPARE --account-name $STORAGE_ACCOUNT_NAME
-az storage container create --name $CONTAINER_NAME_02_EXEC --account-name $STORAGE_ACCOUNT_NAME
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
